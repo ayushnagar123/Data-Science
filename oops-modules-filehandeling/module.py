142161c31a9a610facb73b5aad72d195c9cd93d7
ayushnagar123 <an431999@gmail.com>
2019-08-18 19:58:55 +0530

    modules

---
+++ oops-modules-filehandeling/module.py
@@ -0,0 +1,25 @@
+import math
+
+print(math.factorial(5))
+
+print(math.floor(15/2))
+
+print(math.gcd(12,5))
+
+print(math.log(math.e))
+
+print(math.log(16,2))
+
+print(sum([1,2,3,4,5]))
+
+print(math.pi)
+
+print(math.inf)
+
+print(-math.inf)
+
+print(max([1,2,3,4,5]))
+
+from math import factorial as fact
+print(fact(6))
+
