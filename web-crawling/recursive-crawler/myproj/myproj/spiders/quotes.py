b6c963408e22303056e076b29a5b525928151a05
ayushnagar123 <an431999@gmail.com>
2019-09-09 20:41:50 +0530

    recursive crawler

--- web-crawling/recursive-crawler/myproj/myproj/spiders/quotes.py
+++ web-crawling/recursive-crawler/myproj/myproj/spiders/quotes.py
@@ -7,8 +7,6 @@ class QuotesSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-            'http://quotes.toscrape.com/page/3/'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -21,15 +19,17 @@ class QuotesSpider(scrapy.Spider):
             text = q.css('span.text::text').get()
             author = q.css('small.author::text').get()
             tags = q.css('a.tag::text').getall()
-            print(text)
-            print(author)
-            print(tags)
         
             yield{
                 'text':text,
                 'author':author,
                 'tags':tags,
             }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if(next_page is not None):
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page,callback=self.parse)
         # with open(filename, 'wb') as f:
         #     f.write(response.body)
         # self.log('Saved file %s' % filename)
